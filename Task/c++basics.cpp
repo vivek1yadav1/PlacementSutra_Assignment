cdd08ff84d58b1394fce49e51690adb5157f75c4
vivek1yadav1 <vivek@LAPTOP-U9KDIS92.>
2024-11-19 18:08:39 +0000

    added c++ programs/codes

---
+++ Task/c++basics.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+using namespace std;
+
+ //functions
+    // int factorial(int a){
+    // if (a == 0){
+    //      return 1;
+    // }
+           
+    // else{
+    //     return a * factorial (a-1);    
+
+    //     }
+            
+    // }
+
+
+int main(){
+    //cout << "lets code"<< endl;
+    // int a, b;
+
+    // a = 7;
+    // b = 3;
+   
+   
+    //for taking inputs
+    // cout<<"enter a = "<<endl;
+    // cin>>a;
+    // cout<<"enter b = "<<endl;
+    // cin>>b;
+
+
+    // cout <<"addition is " << a+b <<endl; 
+    // cout <<"multiply is " << a*b <<endl; 
+    // cout <<"subtraction is " << a-b <<endl; 
+    // cout <<"division is " << (float)a/b <<endl; //typecasted
+
+    
+    // int age;
+    // cout << "enter ur age please"<<endl;
+    // cin>>age;
+    // if(age>=18 && age<=100){
+    //     cout<<"u can drive and vote"<<endl;
+    // }
+    // else{
+    //     cout<<"u cant drive and vote"<<endl;
+    // }
+
+    
+    // int n;
+    // cout<<"enter the number to print its table = ";
+    // cin>>n;
+    // for (int i = 1; i<=10 ; i++){
+    //     cout<< i*n<<endl;
+    // }
+
+    // int n;
+    // cout << "enter n = ";
+    // cin>>n;
+    // while(n<101){
+    //     cout<<n<<". vivek invincible"<<endl;
+    //     n = n+1;
+    // }
+
+    // int age;
+    // cout << "enter ur age please"<<endl;
+    // cin>>age;
+    // switch(age){
+    //     case 18 : 
+    //     cout<<"u can"<<endl;
+    //     break;
+    //     case 22 :
+    //     cout << "ur age is 22"<<endl;
+    //     default :
+    //     cout<< "u r neither 18 nor 22";
+    // }
+
+
+    // int n;
+    // cout<<"enter n = ";
+    // cin>>n;
+    // cout<<"factorial of "<<n<<" is "<<factorial(n)<<endl;
+
+
+    // 1D array
+    // int marks[5];
+    // for(int i = 0; i < 5; i++){
+    //     cout<<"enter the mark of "<<i+1<<"st subject"<<endl;
+    //     cin>>marks[i];
+    // }
+    // for(int j = 0; j < 5; j++){
+    //     cout<<"the mark of "<<j+1<<"st subject is "<<marks[j]<<endl;
+    // }
+
+
+    //2D ARRAY
+
+//    int mat1[3][4] = {
+//         {1,2,3,4},
+//         {5,6,7,8},
+//         {9,10,11,12}
+//     };
+//     for (int i=0; i<3; i++){
+//         for(int j=0; j<4; j++)
+//            cout<<"value at "<<i+1<<" "<<j+1<<" is "<<mat1[i][j]<<endl;
+//     }
+
+     int mat1[3][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+    int mat2[3][4] = {
+        {11,12,13,14},
+        {15,16,17,18},
+        {19,110,111,112}
+    };
+    int mat3[3][4];//sum of mat1 + mat2
+    for (int i=0; i<3; i++){
+        for(int j=0; j<4; j++)
+           cout<<"value at "<<i+1<<" "<<j+1<<" of mat3 is "<<mat1[i][j] + mat2[i][j] <<endl;
+    }
+
+
+
+
+
+
+    //   int mat1[3][4] = {
+    //     {1,2,3,4},
+    //     {5,6,7,8},
+    //     {9,10,11,12}
+    // };
+    // int mat2[4][3] = {
+    //     {11,12,13},
+    //     {15,16,17},
+    //     {19,110,111}
+    //     {1,1,1,1}
+    // };
+    // int mat3[3][3];
+    // for (int i=0; i<4; i++){
+    //     for(int j=0; j<3; j++)
+    //     for(int k=0; k<4; k++)
+    //     for(int l=0; l<3; l++)
+    //        cout<<"value at "<<i+1<<" "<<j+1<<" of mat3 is "<< (mat1[i][j] * mat2[k][l]) + mat1[i][j]*mat2[k][l] +  mat1[i][j]*mat2[k][l] <<endl;
+    // }
+
+
+
+    
+
+
+
+    return 0;
+}
+
