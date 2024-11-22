831472ebaec2d69d9df4d3a2fc88ae21c26c70b1
vivek1yadav1 <vivek@LAPTOP-U9KDIS92.>
2024-11-22 14:12:09 +0000

    Q1. solution

--- Task/assignment1.cpp
+++ Task/assignment1.cpp
@@ -7,24 +7,50 @@ using namespace std;
 //      print details of the student using cout.
 //      upload on the git hub.
 
-class Student{
+// class Student{
+//     public:
+//     string name;
+//     int age;
+//     void input(){
+//         cout<<"enter ur name = ";
+//         cin>>name;
+//         cout<<"enter ur age = ";
+//         cin>>age;
+//     }
+// };
+
+
+//Q2. define class Number with one attribute value (integer).
+  // add a member function checkEvenOdd() to check even or odd using if else.
+  // create object assign value to it and call the function.
+
+class Number{
     public:
-    string name;
-    int age;
-    void input(){
-        cout<<"enter ur name = ";
-        cin>>name;
-        cout<<"enter ur age = ";
-        cin>>age;
+    int n;
+    void checkEvenOdd(){
+        if(n%2 == 0){
+            cout<<"entered number "<<n<<" is even."<<endl;
+        }
+        else{
+            cout<<"entered number "<<n<<" is odd."<<endl;
+        }
+
     }
 };
 
 int main(){
    
     //Q1
-    Student Vivek;
-    Vivek.input();
-    cout<<"student name is "<<Vivek.name<<endl;
-    cout<<"student age is "<<Vivek.age<<endl;
+    // Student Vivek;
+    // Vivek.input();
+    // cout<<"student name is "<<Vivek.name<<endl;
+    // cout<<"student age is "<<Vivek.age<<endl;
+    
+    //Q2
+    Number number;
+    cout<<"enter a number = ";
+    cin>>number.n;
+    number.checkEvenOdd();
+    
 
 }
