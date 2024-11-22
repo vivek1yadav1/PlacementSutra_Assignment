f3163f8ec6b93ceea9ce7338a68b9159359c0f8f
vivek1yadav1 <vivek@LAPTOP-U9KDIS92.>
2024-11-22 13:44:04 +0000

    Q1. solution

--- Task/assignment1.cpp
+++ Task/assignment1.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
 
-//Q1. define class student with attribute 
-    //  a.name b.string
-    //  create an object of the student class and assign values to attributes.
-    //  print details of the student using cout.
-    //  upload on the git hub.
+Q1. define class student with attribute 
+     a.name b.string
+     create an object of the student class and assign values to attributes.
+     print details of the student using cout.
+     upload on the git hub.
 
-// class Student{
-//     public:
-//     string name;
-//     int age;
-//     void input(){
-//         cout<<"enter ur name = ";
-//         cin>>name;
-//         cout<<"enter ur age = ";
-//         cin>>age;
-//     }
-// };
+class Student{
+    public:
+    string name;
+    int age;
+    void input(){
+        cout<<"enter ur name = ";
+        cin>>name;
+        cout<<"enter ur age = ";
+        cin>>age;
+    }
+};
 
 int main(){
    
-    //Q1
-    // Student Vivek;
-    // Vivek.input();
-    // cout<<"student name is "<<Vivek.name<<endl;
-    // cout<<"student age is "<<Vivek.age<<endl;
+    Q1
+    Student Vivek;
+    Vivek.input();
+    cout<<"student name is "<<Vivek.name<<endl;
+    cout<<"student age is "<<Vivek.age<<endl;
 
 }
