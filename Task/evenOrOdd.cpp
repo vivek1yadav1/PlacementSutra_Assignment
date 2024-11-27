ce4647188b48ed75b2a92b631a19c7d2de118cf4
vivek1yadav1 <vivek@LAPTOP-U9KDIS92.>
2024-11-27 13:38:03 +0000

    to check entered number is even or odd.

---
+++ Task/evenOrOdd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number = ";
+    cin>>n;
+if(n%2 == 0){
+    cout<<"entered number is even."<<endl;
+}
+else{
+    cout<<"entered number is odd. "<<endl;
+}
+}
